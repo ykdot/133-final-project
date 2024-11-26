98ac66f9e79d572ee53bf7abc0da1443f0d95fce
third <icekap49@gmail.com>
2024-11-26 02:10:13 -0800

    create TaskCard component

---
+++ components/TaskCard/TaskCard.tsx
@@ -0,0 +1,22 @@
+import { TaskObject, TaskPriority } from "@/types/TaskTypes";
+
+function convertTaskPriorityToText(priority: TaskPriority) {
+  if (priority == TaskPriority.High) {
+    return "High Priority";
+  } else if (priority == TaskPriority.Medium) {
+    return "Medium Priority";
+  } else {
+    return "Low Priority";
+  }
+}
+
+export default function TaskCard({ task }: { task: TaskObject }) {
+  const textPriorty = convertTaskPriorityToText(task.priority);
+  return (
+    <li className="flex justify-between p-5 border-t-2 border-x-2 border-solid">
+      <p className="w-3/5">{task.taskName}</p>
+      <p className="w-1/5">{task.date}</p>
+      <p className="w-1/5">{textPriorty}</p>
+    </li>
+  );
+}
