ceb0328da0c9c35888745f45d1b2c8129175a77c
ykdo <kdot147x@gmail.com>
2024-12-10 19:35:26 -0800

    create task interface for TaskObject and enum for the TaskPriority and TaskFormType

--- types/TaskTypes.ts
+++ types/TaskTypes.ts
@@ -1,13 +1,23 @@
+
+// enum for TaskPriority
 export enum TaskPriority {
-  Low = 1,
-  Medium = 2,
-  High = 3,
+  Low = '1',
+  Medium = '2',
+  High = '3',
 }
 
+// interface for TaskObject
 export interface TaskObject {
   id: number;
-  taskName: string;
-  date: string;
+  task_name: string;
+  date: Date;
   priority: TaskPriority;
   label: string;
+  done?: boolean
+}
+
+// enum to differentiate between two different forms
+export enum TaskFormType {
+  NewForm,
+  EditForm
 }
