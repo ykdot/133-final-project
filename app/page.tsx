d77f9a699d2e4c3a69f92ca9ffb76dc3b2329110
ykdo <kdot147x@gmail.com>
2024-12-11 20:56:12 -0800

    Revise minor CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import styles from './page.module.css';
 
 export const metadata: Metadata = {
   title: 'College Digital Assistant',
@@ -11,23 +12,28 @@ export default function Home() {
   return (
     <header
       className={
-        'flex flex-col content-center justify-center px-80 pt-80 gap-3'
+        'flex flex-col content-center justify-center px-30 pt-60 gap-3'
       }
     >
-      <h1 className={'m-auto text-4xl'}>Track Your Academic Goals</h1>
-      <p className={'m-auto'}>
+      <h1 className={'m-auto text-4xl w-2/4 text-center'}>
+        Track Your Academic Goals
+      </h1>
+      <p className={'m-auto w-2/4'}>
         Designed to help students track their daily, weekly and monthly tasks
         and stay organized during their academic year. The assistant aids in
         managing class assignments, extracurricular commitments, note-taking,
         and organizing events.
       </p>
-      <div className="flex gap-3">
-        <Button className={'w-25 m-auto'}>
-          <Link href="/task-list">Create Task List</Link>
-        </Button>
-        <Button className={'w-25 m-auto'}>
-          <Link href="/notes">Take Notes</Link>
-        </Button>
+
+      <div className="w-2/4 m-auto">
+        <div className={styles['button-list']}>
+          <Button className={'w-25 m-auto'}>
+            <Link href="/task-list">Create Task List</Link>
+          </Button>
+          <Button className={'w-25 m-auto'}>
+            <Link href="/notes">Take Notes</Link>
+          </Button>
+        </div>
       </div>
     </header>
   );
